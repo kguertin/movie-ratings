bfafcc241538fd8447d3aa2538dcb3c7f1e08d2e
Kevin <kevin.m.guertin@gmail.com>
2020-05-16 16:24:47 -0400

    add public folder as public file and designed nav bar partial

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const routes = require('./routes/routes');
 
 app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 app.use(routes);
 
