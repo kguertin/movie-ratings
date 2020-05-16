6b6cbb228fb486d649b91be9eeb17292c42433fb
Kevin <kevin.m.guertin@gmail.com>
2020-05-16 13:16:22 -0400

    set up env files

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const fetch = require('fetch');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use('/', (req, res) => {
