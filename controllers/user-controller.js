d52147154102bec49927d5ab91575184b18275c0
Kevin <kevin.m.guertin@gmail.com>
2020-05-19 15:15:45 -0400

    add user controller and connected login and signup rotues

---
+++ controllers/user-controller.js
@@ -0,0 +1,7 @@
+exports.getLogin = (req, res) => {
+  res.send('Login')
+}
+
+exports.getSignUp = (req, res) => {
+  res.send('Sign Up')
+}
--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const movieController = require('../controllers/movie-controller');
+const userController = require('../controllers/user-controller');
 
 router.get('/popular', movieController.getPopular);
 
 router.get('/search-movies', movieController.getSearchResults);
 
-router.get('/movies/:id', movieController.getMovieDetails)
+router.get('/movies/:id', movieController.getMovieDetails);
+
+router.get('/login', userController.getLogin);
+
+router.get('/signup', userController.getSignUp);
 
 router.get('/', movieController.getHome);
 
